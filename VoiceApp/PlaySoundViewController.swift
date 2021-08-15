ffe7673fc6fb0e9052ed9f3a7f765ff48a82b835
Alireza <Alirezabeig@gmail.com>
2021-08-15 09:13:18 -0700

    Fixed the crashing bug

--- VoiceApp/PlaySoundViewController.swift
+++ VoiceApp/PlaySoundViewController.swift
@@ -6,7 +6,47 @@
 //
 
 import UIKit
+import AVFoundation
 
-class PlaySoundViewController: NSObject {
+class PlaySoundViewController: UIViewController{
+    
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var chipButton: UIButton!
+    @IBOutlet weak var varButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
 
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
+    
+    @IBAction func playSound(_ sender: UIButton){
+        print("playing any sound")
+    }
+    
+    @IBAction func stopSound(_ sender: AnyObject){
+        print ("Stop Sound works")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
+        
+    }
+    
+    
 }
